5a58575061bd6ee6c55fe7a4644815cbc416781e
Sean McDonald <spmcdonald1316@gmail.com>
2020-03-08 11:36:01 -0500

    add restful routes

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -1,6 +1,40 @@
 class Api::ContactsController < ApplicationController
-  def first_contact
-    @contact = Contact.first
-    render 'first_contact.json.jb'
+  def show
+    @contact = Contact.find_by(id: params[:id])
+    render 'show.json.jb'
+  end
+
+  def index
+    @contacts = Contact.all
+    render 'index.json.jb'
+  end
+
+  def create
+    @contact = Contact.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:phone_number]
+    )
+    @contact.save
+    render 'show.json.jb'
+  end
+
+  def update
+    @contact = Contact.find_by(id: params[:id])
+    @contact.update(
+      first_name: params[:first_name] || @contact.first_name,
+      last_name: params[:last_name] || @contact.last_name,
+      email: params[:email] || @contact.email,
+      phone_number: params[:phone_number] || @contact.phone_number
+    )
+    @contact.save
+    render 'show.json.jb'
+  end
+
+  def destroy
+    @contact = Contact.find_by(id: params[:id])
+    @contact.destroy
+    render json: {message: "Contact has been removed"}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,14 @@ Rails.application.routes.draw do
   # end
 
   namespace :api do
-    get "/first_contact" => "contacts#first_contact"
+    get "contacts/:id" => "contacts#show"
+
+    get "/contacts" => "contacts#index"
+
+    post "/contacts" => "contacts#create"
+
+    patch "/contacts/:id" => "contacts#update"
+
+    delete "/contacts/:id" => "contacts#destroy"
   end
 end
