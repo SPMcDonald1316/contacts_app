f2ae853e82fccef2b6fb1b02912cd532a2b7a008
Sean McDonald <spmcdonald1316@gmail.com>
2020-03-09 19:37:29 -0500

    build friendly_updated_at model method

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,6 +3,10 @@ class Contact < ApplicationRecord
     created_at.strftime("%d.%m.%y")
   end
 
+  def friendly_updated_at
+    updated_at.strftime("%d.%m.%y")
+  end
+
   def full_name
     "#{first_name} #{last_name}"
   end
