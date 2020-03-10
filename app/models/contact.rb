f307aa1d423b7ec8a6eacfb1bbb22fe2a0c715c3
Sean McDonald <spmcdonald1316@gmail.com>
2020-03-09 19:18:14 -0500

    build model methods for full_name and created_at

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,13 @@
 class Contact < ApplicationRecord
+  def friendly_created_at
+    created_at.strftime("%d.%m.%y")
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def country_code
+    phone_number.prepend("+81 ")
+  end
 end
