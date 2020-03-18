720f01c6b290da4b13a733b18854fa79ddc7a9cf
Sean McDonald <spmcdonald1316@gmail.com>
2020-03-17 19:18:09 -0500

    create user model, add users controller, add create route and method for users

--- config/routes.rb
+++ config/routes.rb
@@ -9,13 +9,10 @@ Rails.application.routes.draw do
 
   namespace :api do
     get "contacts/:id" => "contacts#show"
-
     get "/contacts" => "contacts#index"
-
     post "/contacts" => "contacts#create"
-
     patch "/contacts/:id" => "contacts#update"
-
     delete "/contacts/:id" => "contacts#destroy"
+    post "/users" => "users#create"
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_11_020118) do
+ActiveRecord::Schema.define(version: 2020_03_18_001219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 2020_03_11_020118) do
     t.string "middle_name"
     t.text "bio"
     t.string "address"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
